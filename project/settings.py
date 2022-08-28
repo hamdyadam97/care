4900e5e28c04364b1976616dee66e5561cf4c184
Hamdy Adam <hamdyadam97@gmail.com>
2022-08-28 02:01:36 +0200

    herok

--- project/settings.py
+++ project/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_DIRS = [BASE_DIR/'static/']
+# STATICFILES_DIRS = [BASE_DIR/'static/']
 django_heroku.settings(locals())
 
 # Default primary key field type
